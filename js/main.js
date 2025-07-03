96980ade946a705cb7208b8eecef351876c42bd5
Mohmed Elswady <moelswady99@gmail.com>
2025-07-03 06:56:37 +0300

    Fix Validation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var siteName = document.getElementById('siteName')
 var siteUrl = document.getElementById('siteUrl')
+var nameAlert = document.getElementById('nameAlert')
+var emailAlert = document.getElementById('emailAlert')
+var addSite = document.getElementById('addSite')
+var sitePattern = {
+    siteName:/^[A-Za-z0-9-\s]{3,30}$/,
+    siteUrl:/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%\+.~#?&//=]*)/
+}
 
 var sitesList = []
 
@@ -7,23 +14,75 @@ sitesList = JSON.parse(localStorage.getItem('sites')) || []
 
 displaySite()
 
-var isFormValid = false
+var isNameValid = false
+var isUrlValid = false
+var isInputNameEmpty = false
+var isInputUrlEmpty = false
 
-function addSite(){
-    if (isFormValid){
-        var siteObject = {
-        name: siteName.value,
-        url: siteUrl.value
+function checkNameValidation(){
+     if (sitePattern.siteName.test(siteName.value)){
+        siteName.classList.add('is-valid')
+        siteName.classList.remove('is-invalid')
+        nameAlert.classList.replace('d-block', 'd-none')
+        isNameValid = true
+    }else{
+        siteName.classList.add('is-invalid')
+        nameAlert.classList.replace('d-none', 'd-block')
+        isNameValid = false
     }
 
+    if(siteName.value == ''){
+        isInputNameEmpty = false
+    }else{
+        isInputNameEmpty = true
+    }
+}
+
+function checkUrlValidation(){
+     if(sitePattern.siteUrl.test(siteUrl.value)){
+        siteUrl.classList.add('is-valid')
+        siteUrl.classList.remove('is-invalid')
+        emailAlert.classList.replace('d-block', 'd-none')
+        isUrlValid = true
+    }else{
+        siteUrl.classList.add('is-invalid')
+        emailAlert.classList.replace('d-none', 'd-block')
+        isUrlValid = false
+    }
+
+    if(siteUrl.value == ''){
+        isInputUrlEmpty = false
+    }else{
+        isInputUrlEmpty = true
+    }
+}
+
+addSite.addEventListener('click', function(){
+
+    checkNameValidation()
+    checkUrlValidation()
+
+    if(isInputNameEmpty == false || isInputUrlEmpty == false || isNameValid == false || isUrlValid == false){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Site Name or Url is not valid",
+        });
+    }
+   
+    if (isInputNameEmpty && isInputUrlEmpty &&  isNameValid && isUrlValid){
+        var siteObject = {
+            name: siteName.value,
+            url: siteUrl.value
+        }
+        
     sitesList.push(siteObject)
     localStorage.setItem('sites', JSON.stringify(sitesList))
     displaySite()
     resetInput()
     }
-        deletValidate()
-     
-}
+    deletValidate()
+})
 
 function deletValidate(){
     var nameInput = document.getElementById('siteName')
@@ -67,19 +126,6 @@ function deletSite(index){
     localStorage.setItem('sites', JSON.stringify(sitesList))
 }
 
-var sitePattern = {
-    siteName:/^[A-Za-z0-9-\s]{3,15}$/,
-    siteUrl:/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%\+.~#?&//=]*)/
-}
+siteName.addEventListener('input',checkNameValidation)
 
-function validate(input){
-    if (sitePattern[input.id].test(input.value)){
-        input.classList.add('is-valid')
-        input.classList.remove('is-invalid')
-        isFormValid = true
-    }else{
-        input.classList.add('is-invalid')
-        // input.classList.remov('is-valid')
-        isFormValid = false
-    }
-}
+siteUrl.addEventListener('input',checkUrlValidation)
